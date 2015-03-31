5745c29c8f36583068629e164a3b6b956bbd8f51
nmuelle2 <nina-marie.mueller@web.de>
2015-03-31 11:31:04 +0200

    Remove PCP-Repo as the happiness index is a non-pcp project

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -Dserver.port=$PORT -jar happiness-index-application/target/yana-dash.jar
+web:    java -Dserver.port=$PORT -jar happiness-index-application/target/happiness-index-application-1.0-SNAPSHOT.jar
--- happiness-index-application/src/main/java/de/HappinessIndex/Application.java
+++ happiness-index-application/src/main/java/de/HappinessIndex/Application.java
@@ -1,3 +1,5 @@
+package de.HappinessIndex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
